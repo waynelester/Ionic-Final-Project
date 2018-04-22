1d8a345bca69c7a1d537b3aa0ebcd7cb60bfc60d
Wayne <waynelester89@gmail.com>
2018-04-22 18:29:03 +0000

    maps working in theory, at least on mobile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-
-declare var google;
+ 
+declare var google: any;
  
 @Component({
   selector: 'home-page',
@@ -13,8 +13,7 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
  
-  constructor(public navCtrl: NavController,
-              public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
  
   }
  
@@ -24,39 +23,22 @@ export class HomePage {
  
   loadMap(){
  
-    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    this.geolocation.getCurrentPosition().then((position) => {
  
-    let mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
  
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      console.log(err);
+    });
  
   }
-  addMarker(){
  
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
- 
-  let content = "<h4>Information!</h4>";         
- 
-  this.addInfoWindow(marker, content);
- 
-}
-addInfoWindow(marker, content){
- 
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
- 
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
- 
-}
 }
