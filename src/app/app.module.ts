e84fef09a8b71701b0a6ae1579a187e06724dae9
Wayne Lester <waynelester@Waynes-Air.localdomain>
2018-04-25 18:40:39 -0700

    login and register pages, user provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 import { Camera } from '@ionic-native/camera';
 import { Geolocation } from '@ionic-native/geolocation';
@@ -12,6 +14,7 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MapsProvider } from '../providers/maps/maps';
+import { UserProvider } from '../providers/user/user';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,9 @@ import { MapsProvider } from '../providers/maps/maps';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
@@ -36,7 +41,8 @@ import { MapsProvider } from '../providers/maps/maps';
     MapsProvider,
     Geolocation,
     Camera,
-    MapsProvider
+    MapsProvider,
+    UserProvider
   ]
 })
 export class AppModule {}
